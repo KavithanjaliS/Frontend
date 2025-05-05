a09c04b6bae22213d0f433082ee7ccab76a1c6d5
KavithanjaliS <kavitha211202@gmail.com>
2025-05-05 19:40:58 +0530

    Add files via upload

---
+++ hi.js
@@ -0,0 +1,12 @@
+/*console.log("helloooo");
+ // this is a single line comment
+
+ console.log("outside comment");
+ // this is a statement*/
+
+
+console.log("helloooo");
+ // this is a single line comment
+
+ console.log("outside comment");
+ // this is a statement
