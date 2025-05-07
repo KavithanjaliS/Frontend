bdac34b52de0eef691264c8796e35ad3ad0e031e
KavithanjaliS <kavitha211202@gmail.com>
2025-05-07 19:14:21 +0530

    Add files via upload

---
+++ Operators.js
@@ -0,0 +1,8 @@
+let a=12,b=6;
+let sum=a+b;
+console.log(sum);
+
+let c='11';
+let d='12';
+let add=+a + +b;
+console.log(add);
---
+++ type conversion.js
@@ -0,0 +1,20 @@
+let age=23;
+console.log(typeof age);
+age= String(age);
+console.log(typeof age);
+
+
+console.log('12'/'2');
+
+let count='23';
+console.log(typeof count);
+count= Number(count);
+console.log(typeof count);
+
+
+let x='0';
+console.log(typeof x);
+x=Boolean(x);
+console.log(typeof x);
+console.log(x);
+
